4883d176447352349c84982af8d021137a1634b9
Calvin Barrett <calvinpennbarrett@gmail.com>
2020-08-07 09:23:46 -0700

    Edited dataset pathes when loading kitti disparities

---
+++ bash/ua_hpc_user/bash_evaluate_kitti_ua_25.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+encoder_='vggASPP'
+batch_=8
+epoch_=100
+dataset_='1'
+split_='kitti'
+ckpt_=362500
+lr_loss_=0.25
+
+data_path_='/home/u22/cpbarrett/lw-eg-monodepth/dashcam_frames/'
+
+model_name="$(printf '%s_%s_%dx%d_Ccor_%d' kitti ${encoder_%} ${batch_%} ${epoch_%} 25)"
+echo ">>> ${model_name}"
+
+singularity run --nv /groups/ditzler/envImg/tfcvpy36tf15.img \
+monodepth_main.py --mode test \
+--data_path "$(printf '%s%s/' ${data_path_%} ${dataset_%})" \
+--filenames_file utils/filenames/dash1filenames.txt \
+--log_directory ../log/ \
+--encoder ${encoder_} \
+--checkpoint_path $(printf 'models/%s/model-%d' ${model_name%} ${ckpt_%})
+
+echo ">>> ${backbone%}"
+echo ">>> Kitti: Native Evaluation"
+singularity run --nv /groups/ditzler/envImg/tfcvpy36tf15.img \
+utils/evaluate_kitti.py --split ${split_} \
+--gt_path "$(printf '%s%s/stereo2015/' '/groups/ditzler/' 'kitti')" \
+--predicted_disp_path $(printf 'models/%s/disparities.npy' ${model_name%})
+
+echo ">>> Kitti: Post-Processing Evaluation"
+singularity run --nv /groups/ditzler/envImg/tfcvpy36tf15.img \
+utils/evaluate_kitti.py --split ${split_} \
+--gt_path "$(printf '%s%s/stereo2015/' '/groups/ditzler/' 'kitti')" \
+--predicted_disp_path $(printf 'models/%s/disparities_pp.npy' ${model_name%})
+
+echo ">>> Kitti: Edge-Guided Post-Processing Evaluation"
+singularity run --nv /groups/ditzler/envImg/tfcvpy36tf15.img \
+utils/evaluate_kitti.py --split ${split_} \
+--gt_path "$(printf '%s%s/stereo2015/' '/groups/ditzler/' 'kitti')" \
+--predicted_disp_path $(printf 'models/%s/disparities_ppp.npy' ${model_name%})
+
--- monodepth_main.py
+++ monodepth_main.py
@@ -19,6 +19,7 @@ import re
 import time
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+import sys
 
 from monodepth_model import *
 from monodepth_dataloader import *
@@ -179,6 +180,7 @@ def train(params):
 def test(params):
     """Test function."""
 
+    print("\nLoading images at " + args.data_path+"\n")
     dataloader = MonodepthDataloader(args.data_path, args.filenames_file, params, args.dataset, args.mode)
     left  = dataloader.left_image_batch
     right = dataloader.right_image_batch
@@ -193,6 +195,7 @@ def test(params):
     # SAVER
     train_saver = tf.train.Saver()
 
+
     # INIT
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
@@ -206,7 +209,9 @@ def test(params):
         restore_path = args.checkpoint_path.split(".")[0]
     train_saver.restore(sess, restore_path)
 
-    num_test_samples = count_text_lines(args.filenames_file)
+    # num_test_samples = count_text_lines(args.filenames_file)
+    
+    num_test_samples = 30
 
     print('now testing {} files'.format(num_test_samples))
     disparities     = np.zeros((num_test_samples, params.height, params.width), dtype=np.float32)
